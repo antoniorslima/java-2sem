760956d0f0cc5366d001366cea55714834393725
antoniorslima <antonio.lima@bandtec.com.br>
2022-05-04 14:43:02 -0300

    exercicio agregacao desafio

--- pet-shop-adsb/src/main/java/com/mycompany/pet/shop/adsb/PetShop.java
+++ pet-shop-adsb/src/main/java/com/mycompany/pet/shop/adsb/PetShop.java
@@ -9,21 +9,21 @@ package com.mycompany.pet.shop.adsb;
  * @author aluno
  */
 public class PetShop {
-    
+
     private String nome;
     private Double valorFaturado;
-    
+
     public PetShop(String nome) {
         this.nome = nome;
         this.valorFaturado = 0.0;
     }
-    
+
     public void darBanho(Pet petInformado, Double valorInformado) {
         valorFaturado += valorInformado;
         petInformado.setQtdVisitas(petInformado.getQtdVisitas() + 1);
         petInformado.setValorTotalGasto(petInformado.getValorTotalGasto() + valorInformado);
     }
-    
+
     public void darBanho(Pet petInformado, Double valorInformado, Integer desconto) {
         Double valorDesconto = (desconto / 100.0) * valorInformado;
         Double valorPago = valorInformado - valorDesconto;
@@ -31,23 +31,23 @@ public class PetShop {
         petInformado.setQtdVisitas(petInformado.getQtdVisitas() + 1);
         petInformado.setValorTotalGasto(petInformado.getValorTotalGasto() + valorPago);
     }
-    
+
     public String getNome() {
         return nome;
     }
-    
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
     public Double getValorFaturado() {
         return valorFaturado;
     }
-    
+
     public void setValorFaturado(Double valorFaturado) {
         this.valorFaturado = valorFaturado;
     }
-    
+
     @Override
     public String toString() {
         return String.format("\n\nRelatório de vendas\n"
@@ -55,5 +55,5 @@ public class PetShop {
                 + "\nTotal faturado: %.2f"
                 + "\n----------\n", nome, valorFaturado);
     }
-    
+
 }
